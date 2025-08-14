0337620fe912315d1a345d43e957bd7137ec5020
cassmastern <lernoy@gmail.com>
2025-08-14 20:03:09 +0400

    google analytics, for the grade 5 challenge

---
+++ docs/js/google-analytics.js
@@ -0,0 +1,7 @@
+// Google Analytics 4
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+// GA4 Measurement ID
+gtag('config', 'G-GD2XWDMP2E');
