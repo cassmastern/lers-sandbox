b4ee19e84f1d1dec987200df2abf861fdd52c7b1
cassmastern <lernoy@gmail.com>
2025-08-14 20:22:35 +0400

    GA4 library missing in JS

--- docs/js/google-analytics.js
+++ docs/js/google-analytics.js
@@ -1,7 +1,19 @@
-// Google Analytics 4
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+// load gtag.js
+(function() {
+  // GA4 Measurement ID
+  var GA_MEASUREMENT_ID = 'G-GD2XWDMP2E';
 
-// GA4 Measurement ID
-gtag('config', 'G-GD2XWDMP2E');
+  // Load the GA4 library
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(gtagScript);
+
+  // Initialize gtag function after library loads
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', GA_MEASUREMENT_ID);
+})();
